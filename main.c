c106d20e6b8e00d85c8f8d4d90fe57a2d7f4027d
otumian <popecan1000@gmail.com>
2020-06-28 09:57:31 +0000

    main.c - front-end

--- main.c
+++ main.c
@@ -4,126 +4,54 @@ created on Friday, 2019, 05 July
 By Otumian
 */
 
-/*
-Student record system - with sqlite database
-table schema:
-    id | student_id | student_name | student_program | freshman_date | graduation_date
-*/
-
 #include <stdio.h>
-#include <sqlite3.h>
-
-// i didn't write this callback
-static int callback(void *NotUsed, int argc, char **argv, char **azColName)
-{
-    int i;
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }
-    printf("\n");
-
-    return 0;
-}
 
 int main()
 {
-    // conn to sqlite and open database
-    sqlite3 *conn;
+    unsigned int option;
 
-    // errmsg
-    char *errmsg = 0;
+    printf("Blue-horse Student Record Management System\n");
+    printf("1 create database\n");
+    printf("2 create table\n");
+    printf("3 add data\n");
+    printf("4 read data\n");
+    printf("5 update data\n");
+    printf("6 delete data\n");
+    printf("7 read  all data\n");
+    printf("8 delete all  data\n");
 
-    if (sqlite3_open("test.db", &conn))
+    printf("Enter option: ");
+    scanf("%x", &option);
+
+    switch (option)
     {
-        printf("Database couldn't open\n");
+    case 1:
+        printf("%s", "1 create database\n");
+        break;
+    case 2:
+        printf("%s", "2 create table\n");
+        break;
+    case 3:
+        printf("%s", "3 add data\n");
+        break;
+    case 4:
+        printf("%s", "4 read data\n");
+        break;
+    case 5:
+        printf("%s", "5 update data\n");
+        break;
+    case 6:
+        printf("%s", "6 delete data\n");
+        break;
+    case 7:
+        printf("%s", "7 read  all data\n");
+        break;
+    case 8:
+        printf("%s", "8 delete all  data\n");
+        break;
+    default:
+        printf("%s", "Option not known, please select any of the above\n");
     }
-    else
-    {
-        printf("Database accessed successfully\n");
-    }
-
-    // create a table for student
-    char *create_db_sql = "CREATE TABLE IF NOT EXISTS STUDENT0 (\
-    ID INTEGER PRIMARY KEY AUTOINCREMENT,\
-    STUDENT_NAME CHAR(25) NOT NULL,\
-    STUDENT_ID CHAR(5) NOT NULL,\
-    STUDENT_PROGRAM CHAR(25) NOT NULL,\
-    DATE_OF_COMMENCEMENT DATE DEFAULT (DATE('now')),\
-    DATE_OF_GRADUATION DATE NOT NULL\
-);";
-
-    if (sqlite3_exec(conn, create_db_sql, callback, 0, &errmsg) == SQLITE_OK)
-    {
-        puts("table created");
-    }
-    else
-    {
-        puts("couldn't create table");
-        fprintf(stderr, "%s\n", errmsg);
-    }
-
-    // insert query
-    char student_name[27], student_id[7], student_program[27], date_of_commencement[12];
-    char date_of_graduation[12], insert_sql[500];
-
-    printf("Please provide info to the properties below in order\n");
-
-    // student name
-    printf("Name: ");
-    fgets(student_name, 27, stdin);
-
-    // student id
-    // for now id is of size 5 - change in code above - create_db_sql
-    printf("Index Number: ");
-    fgets(student_id, 7, stdin);
-
-    // student program offered
-    printf("Program: ");
-    fgets(student_program, 27, stdin);
-
-    // student date of freshman
-    // enter date in format YYYY-MM-DD
-    // example: 2025-06-12
-    printf("Date of freshman in format[YYYY-MM-DD]: ");
-    fgets(date_of_commencement, 12, stdin);
-
-    // student date of graduation
-    printf("Date of graduation: ");
-    fgets(date_of_graduation, 12, stdin);
-
-    // use the concept of string builder to form the insert_query
-    sprintf(insert_sql, "INSERT INTO STUDENT0(STUDENT_NAME, STUDENT_ID, STUDENT_PROGRAM, DATE_OF_COMMENCEMENT, DATE_OF_GRADUATION)VALUES('%s', '%s', '%s', '%s', '%s')\n",
-            student_name, student_id, student_program, date_of_commencement, date_of_graduation);
-
-    // printf("%s", insert_sql);
-
-    if (sqlite3_exec(conn, insert_sql, callback, 0, &errmsg) == SQLITE_OK)
-    {
-        puts("data inserted into the table");
-    }
-    else
-    {
-        puts("couldn't insert into table");
-        fprintf(stderr, "%s\n", errmsg);
-    }
-
-    // read data from table
-    char *read_sql = "SELECT * FROM STUDENT0";
-
-    if (sqlite3_exec(conn, read_sql, callback, 0, &errmsg) == SQLITE_OK)
-    {
-        puts("data from select query");
-        fprintf(stdout, "Data from table");
-    }
-    else
-    {
-        puts("couldn't insert into table");
-        fprintf(stderr, "%s\n", errmsg);
-    }
-
-    // close conn
-    sqlite3_close(conn);
 
     return 0;
 }
