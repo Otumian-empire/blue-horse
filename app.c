8929febbe078b04c5c6dca45ebe8d58ae8321470
otumian <popecan1000@gmail.com>
2020-07-15 18:43:17 +0000

    undefrined reference errors

--- app.c
+++ app.c
@@ -1,3 +1,68 @@
 #include <sqlite3.h>
+#include <stdio.h>
 #include "app.h"
 
+sqlite3 *conn;
+
+char *errmsg = 0;
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+{
+    int i;
+    for (i = 0; i < argc; i++)
+    {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+
+    return 0;
+}
+
+int connect_database()
+{
+    int db_status = 0;
+
+    if (!sqlite3_open("test.db", &conn))
+    {
+        db_status = 1;
+    }
+
+    return db_status;
+}
+
+void disconnect_database()
+{
+    sqlite3_close(conn);
+}
+
+int insert_into_tb(char student_name[], char student_id[], char student_program[], char date_of_commencement[], char date_of_graduation[])
+{
+    char *insert_sql;
+    int insert_status = 0;
+
+    sprintf(insert_sql, "INSERT INTO STUDENT0(STUDENT_NAME, STUDENT_ID, STUDENT_PROGRAM, DATE_OF_COMMENCEMENT, DATE_OF_GRADUATION)VALUES('%s', '%s', '%s', '%s', '%s')\n",
+            student_name, student_id, student_program, date_of_commencement, date_of_graduation);
+
+    if (sqlite3_exec(conn, insert_sql, callback, 0, &errmsg) == SQLITE_OK)
+    {
+        insert_status = 1;
+    }
+
+    return insert_status;
+}
+
+void read_all_from_tb()
+{
+    char *read_sql = "SELECT * FROM STUDENT0";
+
+    if (sqlite3_exec(conn, read_sql, callback, 0, &errmsg) == SQLITE_OK)
+    {
+        puts("data from select query");
+        fprintf(stdout, "Data from table");
+    }
+    else
+    {
+        puts("couldn't insert into table");
+        fprintf(stderr, "%s\n", errmsg);
+    }
+}
--- app.h
+++ app.h
@@ -1,31 +1,28 @@
 #ifndef APP_H
 #define APP_H
 
-// conn to sqlite and open database
-static int callback(void *NotUsed, int argc, char **argv, char **azColName);
-
 // create database
-bool connect_database(char db_name[]);
+int connect_database();
+// for the above, the database name is default
+// int connect_database(char db_name[]);
 
 // delete table
-bool disconnect_table(char tb_name[]);
-
-// create table by passing sql query
-bool create_table(char tb_name[], char fields);
-
-// delete database
-bool delete_database(char db_name[]);
+// the connection is global
+void disconnect_database();
 
 // insert
-bool insert_into_tb(char tb_name[], char &fields);
+int insert_into_tb(char student_name[], char student_id[], char student_program[], char date_of_commencement[], char date_of_graduation[]);
 
 // read
-void read_from_tb(char tb_name[], char &fields);
+void read_all_from_tb();
+
+/*
+// read 
+void read_from_tb(char tb_name[], char fields);
+// update
+int update_tb(char tb_name[], char fields);
 
 // delete
-bool delete_from_tb(char tb_name[], char &fields);
-
-// update
-bool update_tb(char tb_name[], char &fields);
-
+int delete_from_tb(char tb_name [], char fields);
+*/
 #endif
